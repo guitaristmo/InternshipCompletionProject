4964d472a2b24195895d34838107161f59b4832f
Moshe <guitaristmo@gmail.com>
2020-11-21 20:42:40 -0500

    Basic SparkDB with quill query

---
+++ src/PersonFetcher.scala
@@ -0,0 +1,44 @@
+import io.getquill.QuillSparkContext._
+import org.apache.spark.sql.SparkSession
+
+class PersonFetcher {
+  case class Language(name: String, dialect: String) extends Embedded
+  case class Country(countryCode: String, language: Language)
+  case class City(countryCode: String, name: String)
+
+  implicit lazy val sqlContext =
+    SparkSession.builder().master("local").appName("spark test").getOrCreate().sqlContext
+  import sqlContext.implicits._
+
+  case class Employee(id: Int, firstName: String, lastName: String)
+  case class EmployeeJobs(employeeID: Int, jobID: Int)
+  case class Job(id: Int, title: String)
+  case class JobDoer(jobTitle: String, employee: String)
+
+  val employees = sqlContext.createDataset(Seq(
+    Employee(0, "John", "Goldman"),
+    Employee(1, "Sam", "GreenBerger"),
+    Employee(2, "Simon", "Blumenthal")
+  ))
+  val jobs = sqlContext.createDataset(Seq(
+    Job(111, "Rebbi"),
+    Job(222, "Administrator"),
+    Job(613, "SeforimCleanup")
+    ))
+  val employeeJobs = sqlContext.createDataset(Seq(
+    EmployeeJobs(0, 111),
+    EmployeeJobs(0, 613),
+    EmployeeJobs(1, 613),
+    EmployeeJobs(2, 222)
+  ))
+
+  val jobWorkers = run{
+    liftQuery(employees)
+      .join(liftQuery(employeeJobs))
+      .on((emp, empJob) => emp.id == empJob.employeeID)
+      .join(liftQuery(jobs))
+      .on((eEj, j) => eEj._2.jobID == j.id)
+      .map(eEjJ => JobDoer(eEjJ._2.title, eEjJ._1._1.firstName+" "+eEjJ._1._1.lastName))
+  }
+}
+
